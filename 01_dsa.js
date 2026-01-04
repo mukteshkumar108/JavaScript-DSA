bb4e5f29edbebe5d23fc7f3ba757ec5f7621d7f7
Muktesh kumar <mukteshkumar108@gmail.com>
2026-01-04 19:23:43 +0530

    Add implementation for printing numbers from N to 1

--- 01_dsa.js
+++ 01_dsa.js
@@ -8,3 +8,10 @@
 
 
 // 2. Print Numbers from N to 1 without changing the loop condition of above question
+// Time Complexity O(n)
+// const num = 10;
+
+// for (let i = 1; i <= num; i++) {
+//   console.log(num - i + 1);
+// }
+

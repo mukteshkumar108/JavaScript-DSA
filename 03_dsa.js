90724d157896e41dba294d8cf3666f43bed82adf
Muktesh kumar <mukteshkumar108@gmail.com>
2026-01-22 22:15:35 +0530

    My first star pattern : )

--- 03_dsa.js
+++ 03_dsa.js
@@ -1,6 +1,9 @@
 const row = 5;
 
-for (let i = 0; i < row; i++) {
-    
-    console.log('*');
+for (let i = 0; i <= row; i++) {
+    let line = " ";
+    for (let j = 1; j <= i; j++) {
+        line = line + '*'
+    }
+    console.log(line);
 }
